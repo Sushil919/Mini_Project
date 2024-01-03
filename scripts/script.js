81406b7246f55e77e3887923ca1e3b08baec5a60
Sushil <sushilkumar811544@gmail.com>
2024-01-03 15:14:12 +0530

    Generated all the html using forEach loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
- const products = [{
+const products = [{
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
     rating: {
@@ -6,7 +6,7 @@
         count: 87
     },
     priceCents: 1090
- },
+},
 {
     image: 'images/products/intermediate-composite-basketball.jpg',
     name: 'Intermediate Size Basketball',
@@ -25,3 +25,58 @@
     },
     priceCents: 799
 }];
+
+products.forEach((product) => {
+    const html = `
+         <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image"
+                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+                Black and Gray Athletic Cotton Socks - 6 Pairs
+            </div>
+
+            <div class="product-rating-container">
+                <img class="product-rating-stars"
+                src="images/ratings/rating-45.png">
+                <div class="product-rating-count link-primary">
+                87
+                </div>
+            </div>
+
+            <div class="product-price">
+                $10.90
+            </div>
+
+            <div class="product-quantity-container">
+                <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+                <img src="images/icons/checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary">
+                Add to Cart
+            </button>
+            </div>
+    `;
+
+    console.log(html);
+});
