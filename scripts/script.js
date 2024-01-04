417981a3bbe060e39274a0ee2f517cd4ca9ceeff
Sushil <sushilkumar811544@gmail.com>
2024-01-04 21:10:56 +0530

    Products adding to cart and combining quantities

--- scripts/script.js
+++ scripts/script.js
@@ -63,10 +63,24 @@ const myCart = document.querySelectorAll('.js-add-to-cart');
 myCart.forEach((button) => {
    button.addEventListener('click',() => {
     const productName = button.dataset.productName;
-    cart.push({
-        productName: productName,
-        quantity: 1
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if(productName === item.productName){
+            matchingItem = item;
+        }
     });
+
+        if(matchingItem){
+            matchingItem.quantity+=1;
+        }
+        else{
+            cart.push({
+                productName: productName,
+                quantity: 1
+            });
+        }
     console.log(cart);
    });
 });
