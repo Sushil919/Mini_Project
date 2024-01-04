0fc525b1dce670cb135e7d0621ac056fcf66e5b9
Sushil <sushilkumar811544@gmail.com>
2024-01-04 21:45:29 +0530

    Make the cart  quantity interactive

--- scripts/script.js
+++ scripts/script.js
@@ -81,6 +81,13 @@ myCart.forEach((button) => {
                 quantity: 1
             });
         }
-    console.log(cart);
+
+        let cartQuantity = 0; 
+        
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
    });
 });
