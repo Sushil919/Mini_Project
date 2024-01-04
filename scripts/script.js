42996ec74f8e7d91b87c2666bc7dbdf4319ad5e0
Sushil <sushilkumar811544@gmail.com>
2024-01-04 21:27:08 +0530

    identify product using Id instead of Name

--- scripts/script.js
+++ scripts/script.js
@@ -48,7 +48,7 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                 Add to Cart
             </button>
             </div>
@@ -62,12 +62,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 const myCart = document.querySelectorAll('.js-add-to-cart');
 myCart.forEach((button) => {
    button.addEventListener('click',() => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-        if(productName === item.productName){
+        if(productId === item.productId){
             matchingItem = item;
         }
     });
@@ -77,7 +77,7 @@ myCart.forEach((button) => {
         }
         else{
             cart.push({
-                productName: productName,
+                productId: productId,
                 quantity: 1
             });
         }
